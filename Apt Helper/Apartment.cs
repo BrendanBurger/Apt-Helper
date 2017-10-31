8ae708ccd9b719dbe7f43d551d962dba504dae21
Burger <burgerb3@upmc.edu>
2017-10-31 15:26:26 -0400

    inital commit

---
+++ Apt Helper/Apartment.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apt_Finder
+{
+    /// <summary>
+    /// Apartments will be wieghted on points to compute a total score
+    /// </summary>
+    class Apartment
+    {
+        //100 max
+        public String name = null; // shorthand
+        public int score = 0;
+        public int commute = 0; // 0 - 10
+        public int price = 0; // 0 - 10 
+        public int utils = 0; // 0 - 10
+        public int looks_in = 0; // 0 - 10
+        public int schools = 0; // 0 - 7
+        public int parking = 0;//0 - 7
+        public int neighborhood = 0; // 1 - 7
+        public int rooms = 0; // 2 - 6 *2 for each
+        public int crime = 0; // 0 - 5
+        public int yard = 0; //  0 - 5
+        public int bonus = 0; // 0 - 5 
+        public int hills = 0; // 1 - 5
+        public int sqrSpace = 0; // 0 - 5
+        public int starting = 0; // 1 - 5
+        public int floors = 1; // 1 - 2
+        public int house = 0; // 0 - 1
+        
+
+
+
+        public Apartment(string name)
+        {
+            this.name = name;
+        }
+
+        public void computeScore()
+        {
+            score = price + commute + yard + schools + looks_in + utils + parking + rooms + bonus + neighborhood + hills + starting + floors + crime + house + sqrSpace;
+        }
+
+        public void print()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine("Commute" + commute);
+            Console.WriteLine("Price" + price);
+            Console.WriteLine("Yard" + yard);
+            Console.WriteLine("Schools" + schools);
+            Console.WriteLine("Looks in" + looks_in);
+            Console.WriteLine("Utils" + utils);
+            Console.WriteLine("parking " + parking);
+            Console.WriteLine("Rooms " + rooms);
+            Console.WriteLine("Bonus " + bonus);
+            Console.WriteLine("Neighborhood " + neighborhood);
+            Console.WriteLine("Hills " + hills);
+            Console.WriteLine("Starting " + starting);
+            Console.WriteLine("floors " + floors);
+            Console.WriteLine("Crimes " + crime);
+            Console.WriteLine("house " + house);
+            Console.WriteLine("Score " + score);
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Apt Helper/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apt_Finder
+{
+    class Program
+
+
+    {
+        public static List<Apartment> apts = new List<Apartment>();
+
+        //Get top 5
+        public void Sort()
+        {
+            apts.OrderBy(p => p.score);
+
+        }
+        static void Main(string[] args)
+        {
+           for (int i = 0; i < 32; i++)
+            {
+                Console.WriteLine("Street name");
+                String temp = Console.ReadLine();
+                Apartment a = new Apartment(temp);
+
+                Console.WriteLine("enter commute max: 10");
+                int temp2 = Int32.Parse(Console.ReadLine());
+                a.commute = temp2;
+
+
+                Console.WriteLine("enter price max:10");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.price = temp2;
+
+
+                Console.WriteLine("enter yard max:5");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.yard = temp2;
+
+
+                Console.WriteLine("enter schools max:7");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.schools = temp2;
+
+
+                Console.WriteLine("enter Looks max:10 ");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.looks_in = temp2;
+
+
+                Console.WriteLine("enter utils max:10");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.utils = temp2;
+
+
+                Console.WriteLine("enter parking max:7");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.parking = temp2;
+
+             
+                Console.WriteLine("enter rooms max:3");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.rooms = temp2;
+
+
+                Console.WriteLine("enter bonus max:5");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.bonus = temp2;
+
+
+                Console.WriteLine("enter neighborhood max:5");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.neighborhood = temp2;
+
+
+                Console.WriteLine("enter hills max:5");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.hills = temp2;
+
+
+                Console.WriteLine("enter starting max :5");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.starting = temp2;
+
+
+                Console.WriteLine("enter floors max :2");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.floors = temp2;
+
+
+                Console.WriteLine("enter crime max :10");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.crime = temp2;
+
+
+                Console.WriteLine("enter house max:1");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.house = temp2;
+
+                Console.WriteLine("enter square space max:5");
+                temp2 = Int32.Parse(Console.ReadLine());
+                a.sqrSpace = temp2;
+                a.computeScore();
+                a.print();
+                apts.Add(a);
+           }
+
+
+            apts.Sort();
+
+
+            for (int i = 0; i<4;  i++)
+            {
+                Console.WriteLine(i + 1 + apts.ElementAt(i).name + apts.ElementAt(i).score);
+            }
+
+            }
+        }
+
+    }
+
+ 
